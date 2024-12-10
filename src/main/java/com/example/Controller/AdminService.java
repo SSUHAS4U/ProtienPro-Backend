2d143c8b519df93dabcdf72fb0443f078a7cdafd
S SUHAS <155213119+SSUHAS4U@users.noreply.github.com>
2024-12-10 16:32:43 +0530

    Update AdminService.java

--- src/main/java/com/example/Controller/AdminService.java
+++ src/main/java/com/example/Controller/AdminService.java
@@ -19,7 +19,6 @@ import com.example.Model.Admin;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class AdminService {
 
     @Autowired
