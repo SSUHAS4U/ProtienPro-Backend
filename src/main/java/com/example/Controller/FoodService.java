c769f69fdd4c5528c8c4f6837d4e7c9ea46ce8e8
S SUHAS <155213119+SSUHAS4U@users.noreply.github.com>
2024-12-10 16:52:14 +0530

    Update FoodService.java

--- src/main/java/com/example/Controller/FoodService.java
+++ src/main/java/com/example/Controller/FoodService.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/food")
 public class FoodService {
